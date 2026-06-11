bb821c85b8bc724605a2ccf3eae4cb990edc6584
ammarlaw143-ai <ammarlaw143@gmail.com>
2026-06-11 11:22:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-function minecraftHelper(question){
-  const q = question.toLowerCase();
+function searchContent() {
 
-  if(q.includes("diamond")){
-    return "Diamonds are commonly found below Y level -59.";
-  }
+let input =
+document.getElementById("searchBar")
+.value.toLowerCase();
 
-  if(q.includes("teleport")){
-    return "Use /tp @p x y z";
-  }
+let cards =
+document.querySelectorAll(".card");
 
-  return "Try asking about commands, diamonds, teleporting, mobs, or enchantments.";
+cards.forEach(card => {
+
+if(card.innerText
+.toLowerCase()
+.includes(input))
+{
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+}
+
+function chatbot(){
+
+let q =
+document.getElementById("userInput")
+.value.toLowerCase();
+
+let answer="";
+
+if(q.includes("diamond")){
+answer="Diamonds are commonly found deep underground.";
+}
+else if(q.includes("teleport")){
+answer="Use /tp @p x y z";
+}
+else if(q.includes("creeper")){
+answer="Creepers explode when close to players.";
+}
+else{
+answer="Try asking about diamonds, teleporting, creepers, enchantments or commands.";
+}
+
+document.getElementById("chatOutput")
+.innerHTML=answer;
+
 }
